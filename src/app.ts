ab8f2f40abe9aa8b82efce6bc19d37bc79d766ea
Rafał Świerkot <rafal.swierkot@siili.com>
2020-05-18 09:44:25 +0200

    Configured graphQL error messages.

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import graphqlSchema from './graphql/schema';
 import { container } from "./di/inversify.config";
 import {TYPES} from "./di/types";
 import {IGraphqlResolver} from "./graphql/resolvers/resolvers";
+import { GraphQLError } from "graphql";
+import {HttpError} from "./utils/HttpError";
 
 
 const app = express();
@@ -14,6 +16,15 @@ app.use('/graphql', graphqlHttp({
     schema: graphqlSchema,
     rootValue: resolver,
     graphiql: true,
+    customFormatErrorFn(err: GraphQLError) {
+        if (!err.originalError || !(err.originalError instanceof HttpError) ){
+            return err;
+        }
+        const httpError = err.originalError;
+        const message = httpError.message || 'An error occured';
+        const code = httpError.statusCode || 500;
+        return { message: message, status: code};
+    }
 }))
 
 const port = process.env.TTT_PORT || 8080;
